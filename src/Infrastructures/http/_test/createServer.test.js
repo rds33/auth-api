f0a4cfe1dd91bc37ef5b89313aceb0988bc195a5
rds33 <robbysimbolon41@gmail.com>
2023-10-03 22:56:26 +0700

    add say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -1,9 +1,8 @@
 const pool = require('../../database/postgres/pool');
 const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
 const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper');
-const container = require('../../container');
+const injections = require('../../injections');
 const createServer = require('../createServer');
-const AuthenticationTokenManager = require('../../../Applications/security/AuthenticationTokenManager');
 
 describe('HTTP server', () => {
   afterAll(async () => {
@@ -27,6 +26,22 @@ describe('HTTP server', () => {
 
     // Assert
     expect(response.statusCode).toEqual(404);
+  });
+
+  describe('when GET /', () => {
+    it('should return 200 and hello world', async () => {
+      // Arrange
+      const server = await createServer({});
+      // Action
+      const response = await server.inject({
+        method: 'GET',
+        url: '/',
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.value).toEqual('Hello world!');
+    });
   });
 
   describe('when POST /users', () => {
